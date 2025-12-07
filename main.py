6170e233b10d5c87e5e41ba8df7e673f70c722eb
Ishita <ishitaj232@gmail.com>
2025-12-07 18:49:12 +0530

    prepare base model added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME = "Data ingestion stage"
 try:
@@ -8,6 +9,18 @@ try:
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
+STAGE_NAME = "Prepare base model stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = PrepareBaseModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")      
 except Exception as e:
     logger.exception(e)
     raise e
--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -9,3 +9,14 @@ class DataIngestionConfig:
     source_URL : str
     local_data_file : Path
     unzip_dir : Path
+
+@dataclass(frozen=True) #frozen=true means i dont want to add anything else in this
+class PrepareBaseModelConfig:
+    root_dir:Path
+    base_model_path:Path
+    updated_base_model_path:Path
+    params_image_size: list
+    params_learning_rate: float
+    params_classes: int
+    params_weights: str
+    params_include_top: bool
